3147d84532d68dffe6466dd13efc44817362fe3b
Isaac Woods <isaacwoods.home@gmail.com>
2016-10-01 15:27:10 +0100

    Start emitting AIR instructions and labels

--- src/air.cpp
+++ src/air.cpp
@@ -13,6 +13,9 @@ static air_instruction* CreateInstruction(instruction_type type, ...)
   i->type = type;
   i->next = nullptr;
 
+  va_list args;
+  va_start(args, type);
+
   switch (type)
   {
     case I_ENTER_STACK_FRAME:
@@ -23,6 +26,16 @@ static air_instruction* CreateInstruction(instruction_type type, ...)
     {
     } break;
 
+    case I_RETURN:
+    {
+    } break;
+
+    case I_JUMP:
+    {
+      i->payload.jump.cond                    = static_cast<jump_instruction_part::condition(va_arg(args, int));
+      i->payload.jump.labelInstruction        = va_arg(args, air_instruction*);
+    } break;
+
     default:
     {
       fprintf(stderr, "Unhandled AIR instruction type in CreateInstruction!\n");
@@ -30,24 +43,122 @@ static air_instruction* CreateInstruction(instruction_type type, ...)
     }
   }
 
+  va_end(args);
   return i;
 }
 
-void GenerateAIRForFunction(function_def* function)
+#define PUSH(instruction) \
+  tail->next = instruction; \
+  tail = tail->next;
+
+static void GenNodeAIR(air_instruction* tail, node* n)
+{
+  assert(n != nullptr);
+
+  switch (n->type)
+  {
+    case BREAK_NODE:
+    {
+      instruction_label* label = static_cast<instruction_label*>(malloc(sizeof(instruction_label)));
+      // TODO: find the end of the loop and attach the label
+
+      PUSH(CreateInstruction(I_JUMP, UNCONDITIONAL, label));
+    } break;
+
+    case RETURN_NODE:
+    {
+      PUSH(CreateInstruction(I_LEAVE_STACK_FRAME));
+      PUSH(CreateInstruction(I_RETURN));
+    } break;
+
+    case BINARY_OP_NODE:
+    {
+
+    } break;
+
+    case PREFIX_OP_NODE:
+    {
+
+    } break;
+
+    case VARIABLE_NODE:
+    {
+
+    };
+
+    case CONDITION_NODE:
+    {
+    } break;
+
+    case IF_NODE:
+    {
+
+    } break;
+
+    case NUMBER_CONSTANT_NODE:
+    {
+
+    } break;
+
+    case STRING_CONSTANT_NODE:
+    {
+
+    } break;
+
+    case FUNCTION_CALL_NODE:
+    {
+
+    } break;
+
+    case FUNCTION_CALL_NODE:
+    {
+
+    } break;
+
+    case VARIABLE_ASSIGN_NODE:
+    {
+      slot* variableSlot = static_cast<slot*>(malloc(sizeof(slot)));
+      // TODO
+
+      slot* expressionSlot = static_cast<slot*>(malloc(sizeof(slot)));
+      // TODO
+
+      PUSH(CreateInstruction(I_MOV, variableSlot, expressionSlot));
+    } break;
+
+    default:
+    {
+      fprintf("Unhandled AST node type in GenNodeAIR!\n");
+      exit(1);
+    }
+  }
+}
+
+void GenFunctionAIR(function_def* function)
 {
   assert(function->code == nullptr);
+
   function->code = CreateInstruction(I_ENTER_STACK_FRAME);
   air_instruction* tail = function->code;
 
-  #define PUSH(instruction) \
-    tail->next = instruction; \
-    tail = tail->next;
+  for (node* n = function->ast;
+       n;
+       n = n->next)
+  {
+    GenNodeAIR(tail, n);
+  }
 
   PUSH(CreateInstruction(I_LEAVE_STACK_FRAME));
 }
 
 void PrintInstruction(air_instruction* instruction)
 {
+  if (instruction->label)
+  {
+    // TODO(Isaac): print more information about the label
+    printf("[LABEL]: ");
+  }
+
   switch (instruction->type)
   {
     case I_ENTER_STACK_FRAME:
@@ -68,8 +179,18 @@ void PrintInstruction(air_instruction* instruction)
   }
 }
 
+static void FreeInstructionLabel(instruction_label* label)
+{
+  free(label);
+}
+
 void FreeInstruction(air_instruction* instruction)
 {
+  if (instruction->label)
+  {
+    FreeInstructionLabel(instruction->label);
+  }
+
   switch (instruction->type)
   {
     case I_ENTER_STACK_FRAME:
@@ -77,6 +198,14 @@ void FreeInstruction(air_instruction* instruction)
     } break;
 
     case I_LEAVE_STACK_FRAME:
+    {
+    } break;
+
+    case I_RETURN:
+    {
+    } break;
+
+    case I_JUMP:
     {
     } break;
 
--- src/air.hpp
+++ src/air.hpp
@@ -10,17 +10,69 @@ enum instruction_type
 {
   I_ENTER_STACK_FRAME,
   I_LEAVE_STACK_FRAME,
+  I_RETURN,
+  I_JUMP,
+  I_MOV,
 
   NUM_INSTRUCTIONS
 };
 
-struct air_instruction
+struct instruction_label
 {
-  instruction_type  type;
-
-  air_instruction*  next;
+  
 };
 
-void GenerateAIRForFunction(function_def* function);
+struct slot
+{
+  enum slot_type
+  {
+    PARAM,
+    LOCAL,
+  } type;
+
+  variable_def* variableDef;
+  unsigned int  tag;
+};
+
+struct jump_instruction_part
+{
+  enum condition
+  {
+    UNCONDITIONAL,
+    IF_EQUAL,
+    IF_NOT_EQUAL,
+    IF_OVERFLOW,
+    IF_NOT_OVERFLOW,
+    IF_SIGN,
+    IF_NOT_SIGN,
+    IF_LESSER,
+    IF_GREATER,
+    IF_PARITY_EVEN,
+    IF_PARITY_ODD
+  } cond;
+
+  const instruction_label* label;
+};
+
+struct mov_instruction_part
+{
+  slot* a;
+  slot* b;
+};
+
+struct air_instruction
+{
+  air_instruction*      next;
+
+  instruction_type      type;
+  instruction_label*    label;    // NOTE(Isaac): `nullptr` for unlabelled instructions
+
+  union
+  {
+    jump_instruction_part jump;
+  } payload;
+};
+
+void GenFunctionAIR(function_def* function);
 void PrintInstruction(air_instruction* instruction);
 void FreeInstruction(air_instruction* instruction);
